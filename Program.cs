03b1780310da5b5d60fd6c650f5c2e1f71a302a6
ArtemKosholkin <kaa@sdexport.ru>
2024-09-26 12:52:08 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myapples = 5;
+            var hisapples = 6;
+            var hispeares = 5;
+
+            var result = (myapples != hispeares) & (myapples < hisapples);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
