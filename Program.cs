72e501714eabf0a4277315ceadd7bac972f20b6e
ArtemKosholkin <kaa@sdexport.ru>
2024-09-30 15:20:33 +0700

    colors

--- Program.cs
+++ Program.cs
@@ -75,7 +75,7 @@ namespace Module_4
 		
 		var color = Console.ReadLine();
 
-		if (color == "red")
+		/*if (color == "red")
 		{
 			Console.BackgroundColor = ConsoleColor.Red;
 			Console.ForegroundColor = ConsoleColor.Black;
@@ -96,7 +96,39 @@ namespace Module_4
 			Console.ForegroundColor = ConsoleColor.Black;
 
 			Console.WriteLine("Your color is cyan!");
-		}
+		}*/
+
+			switch(color)
+            {
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is red!");
+					break;
+
+					case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is green!");
+					break;
+
+					case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
+
+					default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is yellow!");
+					break;
+            }
+
 		Console.ReadKey();
 
         }
