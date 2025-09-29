a902b65fe31d05d4e076196197e2bac6a996e1b3
kaa <kaa@sdexport.ru>
2025-09-29 17:42:39 +0700

    Урок 4.1. Начало.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace Module4
     {
         static void Main(string[] args)
         {
+            //Урок 4.1
 
+            //var myapples = 5;
+            //var hisapples = 6;
+            //var hispeares = 5;
+            //var result = (myapples != hispeares) & (myapples < hisapples);
+            //Console.WriteLine(result);
+
+            //int a;
+            //int b;
+            //double x;
+            //double y;
+            //bool c = (a < b) | (x > y);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Для продолжения нажмите любую кнопку");
+            Console.ReadKey();
         }
     }
 }
