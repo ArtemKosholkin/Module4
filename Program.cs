72a84977afccc2fb9b4ceb1e62e69a4c5853ffcb
kaa <kaa@sdexport.ru>
2025-10-01 12:19:06 +0700

    Задание 4.1.16 и 17

--- Program.cs
+++ Program.cs
@@ -46,22 +46,58 @@ namespace Module4
             //Console.WriteLine(result);
 
             //Задание 4.1.14
-            var a = 6;
-            var b = 7;
+            //var a = 6;
+            //var b = 7;
 
-            if (a == b && b > 1)
+            //if (a == b && b > 1)
+            //{
+            //    Console.WriteLine("Условие истинно");
+            //}
+            //else if (b > 10 || b == 7)
+            //{
+            //    Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Значение b = {0}", b);
+            //}
+
+            //Задание 4.1.15
+            //var a = 6;
+            //var b = 7;
+            //var c = a != b ? a + b : b;
+            //Console.WriteLine(c);
+
+            //Задание 4.1.16/4.1.17
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы:");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
             {
-                Console.WriteLine("Условие истинно");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш любимый цвет красный!");
             }
-            else if (b > 10 || b == 7)
+            else if (color == "green")
             {
-                Console.WriteLine("Значение b = {0} больше 10 или равно 7", b);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш любимый цвет зеленый!");
             }
             else
             {
-                Console.WriteLine("Значение b = {0}", b);
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш любимый цвет бирюзовый!");
             }
-            
+
+
+
+
 
             Console.WriteLine();
             Console.WriteLine("Для продолжения нажмите любую кнопку");
