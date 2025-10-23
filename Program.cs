79b1793701309b03ea102142638c2ad036aec5bb
kaa <kaa@sdexport.ru>
2025-10-23 12:27:45 +0700

    задание 4.3.11

--- Program.cs
+++ Program.cs
@@ -12,14 +12,44 @@ namespace Module4
         {
             //Урок 4.3
 
-            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
+            //Задание 4.3.12 - отсортировать массив в порядке возрастания цифр
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
-            {
-                for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
-                    Console.Write(array[i, k] + " ");
-                    Console.WriteLine();
-            }
+
+
+
+            //int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+            //Задание 4.3.11 - перебор по столбцам
+            //for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+            //{
+            //    for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            //        Console.Write(array[i, k] + " ");
+            //    Console.WriteLine();
+            //}
+            //ещё вариант:
+            //for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+            //        Console.Write(array[k, i] + " ");
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+            //        Console.Write(array[i, k] + " ");
+            //    Console.WriteLine();
+            //}
+
+            //int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            //for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+            //{
+            //    for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+            //        Console.Write(array[i, k] + " ");
+            //        Console.WriteLine();
+            //}
 
             //определяем количество строк (индекс последнего элемента первой (0) размерности
             //Console.Write("Количество строк: ");
